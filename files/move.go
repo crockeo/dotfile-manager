5c8e39fc8451013e6479a598141de9596ae4291a
Cerek Hillen <crockeo@gmail.com>
2014-12-12 13:33:54 -0500

    Added a function to move.

---
+++ files/move.go
@@ -0,0 +1,10 @@
+// Name: move.go
+// Desc:
+//   This file provides the function to move a file to another location.
+package files
+
+// Function Move moves a file from one place to another.
+func Move(src, dst string) error {
+	// TODO: Complete
+	return nil
+}
