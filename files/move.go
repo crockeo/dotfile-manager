23f43e854234de1a02b5a2f1277d5320700e3fe1
Cerek Hillen <crockeo@gmail.com>
2014-12-12 14:13:15 -0500

    Implemented a function to move a file from one place to another.

--- files/move.go
+++ files/move.go
@@ -3,8 +3,19 @@
 //   This file provides the function to move a file to another location.
 package files
 
-// Function Move moves a file from one place to another.
+import (
+	"errors"
+	"os/exec"
+)
+
+// This function moves a file from one place to another.
 func Move(src, dst string) error {
-	// TODO: Complete
+	cmd := exec.Command("mv", "src", "dst")
+
+	err := cmd.Run()
+	if err != nil {
+		return errors.New("Failed to move the file.")
+	}
+
 	return nil
 }
